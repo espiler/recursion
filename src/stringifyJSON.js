2737de2c4e13ae81c96d540049207b0092d20574
Evan Spiler <monkeysrawesome@gmail.com>
2014-09-22 11:15:48 -0700

    string and object almost done, no recursion yet

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,16 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var res = '';
+  if (typeof(obj) == 'string') {
+    res = res + obj;
+  }
+  if (typeof(obj) == 'object') {
+  	res = res + '{'
+  	for (var key in obj) {
+  		res = res + '"' + key + '":' + obj[key] + ',';
+  	}
+  	res = res + '}'
+  }
+  return res;
 };
